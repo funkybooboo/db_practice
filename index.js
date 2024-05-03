b435dffcee897ab13581c92f76035aac83677444
Nate Stott <nate.stott@pm.me>
2024-05-03 14:34:44 -0600

    set up mongodb

---
+++ index.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost/playground")
+    .then(() => console.log("Connected to MongoDB..."))
+    .catch(error => console.error("Could not connect to MongoDB... ", error.message));
+
+
